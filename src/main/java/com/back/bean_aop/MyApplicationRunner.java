06556070260c1c7474318e55d86fd53f5ee1f432
kjh3165 <jhwk109@gmail.com>
2026-05-04 15:40:35 +0900

    ApplicationRunner는 스프링부트 시작시 초반에 자동으로 run 메서드가 수행됨

---
+++ src/main/java/com/back/bean_aop/MyApplicationRunner.java
@@ -0,0 +1,12 @@
+package com.back.bean_aop;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class MyApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("MyApplicationRunner is running");
+    }
+}
