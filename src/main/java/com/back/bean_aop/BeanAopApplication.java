2dc6cf326b090d153f6d65a30c7ffc22df72ca3e
kjh3165 <jhwk109@gmail.com>
2026-05-04 14:14:23 +0900

    세팅

---
+++ src/main/java/com/back/bean_aop/BeanAopApplication.java
@@ -0,0 +1,13 @@
+package com.back.bean_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanAopApplication.class, args);
+    }
+
+}
