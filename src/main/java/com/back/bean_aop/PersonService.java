a4bf16bc65e9ffe10c500973bb636d67ad053840
kjh3165 <jhwk109@gmail.com>
2026-05-04 15:15:16 +0900

    @Bean을 사용해서 같은 타입의 빈을 이름을 달리하여 2개 등록할 수 있음

--- src/main/java/com/back/bean_aop/PersonService.java
+++ src/main/java/com/back/bean_aop/PersonService.java
@@ -1,11 +1,13 @@
 package com.back.bean_aop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class PersonService {
-    private long i = 0;
+    private final PersonRepository personRepositoryV2;
     public long count() {
-        return i++;
+        return personRepositoryV2.count();
     }
 }
