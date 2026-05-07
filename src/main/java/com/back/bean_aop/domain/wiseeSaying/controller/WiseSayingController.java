30aeca86255021f5704fe082722284d08c9bfdb2
kjh3165 <jhwk109@gmail.com>
2026-05-07 15:12:59 +0900

    명언앱 구현 시작, 등록, 조회, 삭제 기능 추가

---
+++ src/main/java/com/back/bean_aop/domain/wiseeSaying/controller/WiseSayingController.java
@@ -0,0 +1,71 @@
+package com.back.bean_aop.domain.wiseeSaying.controller;
+
+import com.back.bean_aop.domain.wiseeSaying.entity.WiseSaying;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Controller
+public class WiseSayingController {
+    private int lastId = 0;
+    private final List<WiseSaying> wiseSayings = new ArrayList<>();
+
+    @GetMapping("/wiseSayings/write")
+    @ResponseBody
+    public String write(String content, String author) {
+
+        if (content == null || content.isBlank()) {
+            throw new IllegalArgumentException("content is empty");
+        }
+
+        if (author == null || author.isBlank()) {
+            throw  new IllegalArgumentException("author is empty");
+        }
+
+        int id = ++lastId;
+
+        WiseSaying wiseSaying = new WiseSaying(id, content, author);
+
+        wiseSayings.add(wiseSaying);
+
+        return "%d번 명언이 생성되었습니다.".formatted(id);
+    }
+
+    @GetMapping("/wiseSayings")
+    @ResponseBody
+    public String list() {
+        return "<ul>"
+                + wiseSayings
+                .stream()
+                .map(wiseSaying ->
+                        "<li>%d / %s / %s </li>".formatted(wiseSaying.getId(), wiseSaying.getContent(), wiseSaying.getAuthor())
+                )
+                .collect(Collectors.joining(""))
+                + "</ul>";
+    }
+
+    @GetMapping("/wiseSayings/delete/{id}")
+    @ResponseBody
+    public String delete(@PathVariable int id) {
+        WiseSaying wiseSaying = findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("%d번 명언은 존재하지 않습니다.".formatted(id))
+                );
+
+        wiseSayings.remove(wiseSaying);
+
+        return "%d 번 명언이 삭제되었습니다.".formatted(id);
+    }
+
+    private Optional<WiseSaying> findById(int id) {
+        return wiseSayings
+                .stream()
+                .filter(wiseSaying -> wiseSaying.getId() == id)
+                .findFirst();
+    }
+}
---
+++ src/main/java/com/back/bean_aop/domain/wiseeSaying/entity/WiseSaying.java
@@ -0,0 +1,12 @@
+package com.back.bean_aop.domain.wiseeSaying.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+}
