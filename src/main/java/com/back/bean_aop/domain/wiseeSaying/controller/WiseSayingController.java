8aab4ea3f957133c437f18c1a18dd656a9212fb7
kjh3165 <jhwk109@gmail.com>
2026-05-08 09:30:45 +0900

    HtmlRenderer와 Parser를 빈으로 등록

--- src/main/java/com/back/bean_aop/domain/wiseeSaying/controller/WiseSayingController.java
+++ src/main/java/com/back/bean_aop/domain/wiseeSaying/controller/WiseSayingController.java
@@ -18,6 +18,8 @@ import java.util.stream.Collectors;
 @Controller
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
+    private final HtmlRenderer htmlRenderer;
+    private final Parser parser;
 
     @GetMapping("/wiseSayings/write")
     @ResponseBody
@@ -52,24 +54,18 @@ public class WiseSayingController {
                 + "</ul>";
     }
 
-
     @GetMapping("/wiseSayings/{id}")
     @ResponseBody
     public String detail(@PathVariable int id) {
         WiseSaying wiseSaying = wiseSayingService.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("%d번 명언은 존재하지 않습니다.".formatted(id))
                 );
-        // 마크다운 파서 생성
-        Parser parser = Parser.builder().build();
 
         // 문자열을 파싱해서 Node 트리 구조로 변환
         Node document = parser.parse(wiseSaying.getContent());
 
-        // HTML 렌더러 생성
-        HtmlRenderer renderer = HtmlRenderer.builder().build();
-
         // Node를 HTML 문자열로 렌더링
-        String html = renderer.render(document);
+        String html = htmlRenderer.render(document);
 
         return """
                 <h1>번호 : %d</h1>
@@ -77,7 +73,6 @@ public class WiseSayingController {
                 <div>%s</div>
                 """.formatted(wiseSaying.getId(), wiseSaying.getAuthor(), html);
     }
-
 
     @GetMapping("/wiseSayings/{id}/delete")
     @ResponseBody
